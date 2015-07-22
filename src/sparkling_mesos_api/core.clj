b032c94dc29744950214dbf45db97ad2e5db8cd6
Prasanna Gautam <prasannagautam@gmail.com>
2015-07-22 02:34:22 -0700

    Spark initialization

--- src/sparkling_mesos_api/core.clj
+++ src/sparkling_mesos_api/core.clj
@@ -1,6 +1,20 @@
-(ns sparkling-mesos-api.core)
+(ns sparkling-mesos-api.core
+  (:require [clojure.string :as string]
+            [sparkling.conf :as conf]
+            [sparkling.core :as spark]
+            [sparkling.destructuring :as s-de]
+            [sparkling.deserialization])
+  (:import [org.apache.spark SparkConf])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn make-spark-context []
+  (let [sconfig (config/spark)
+        c (->
+           (SparkConf.)
+           (conf/master (:master sconfig))
+           (conf/app-name (:app-name sconfig))
+           (conf/set "spark.executor.uri" (:executor-uri sconfig))
+           (conf/set "spark.ui.port" (:webui-port sconfig))
+           (conf/set "spark.home" (:home sconfig)))]
+    (spark/spark-context c)))
